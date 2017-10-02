6981b82f8b3c6e5d2806969242e8744a40c3799d
Bryce King <brycesk5@gmail.com>
2017-10-02 10:52:21 -0600

    Implement not_string

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -8,7 +8,13 @@ def has_teen?(a, b, c)
     end
 end
 # TODO - write not_string
-
+def not_string(str)
+    if str[0..2].downcase == "not"
+        return str.downcase
+    else
+        return "not" + str.downcase
+    end
+end
 # TODO - write icy_hot?
 
 # TODO - write closer_to
