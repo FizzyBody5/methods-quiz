5ca161a2aa368ffbc9e6ecf8e45700d77d2393eb
Bryce King <brycesk5@gmail.com>
2017-10-02 10:40:27 -0600

    Tests for has_teen?

--- test.rb
+++ test.rb
@@ -2,4 +2,16 @@ require './test_setup'
 require './methods_quiz'
 
 # TODO - write tests here
+describe 'methods-final' do
 
+  describe 'has_teen' do
+    it 'one has teen' do 
+      has_teen?(11, 12, 13).must_equal(true)
+    end
+
+    it 'doesnt have teen' do
+      has_teen?(10, 11, 12).must_equal(false)
+    end
+  end
+
+end
